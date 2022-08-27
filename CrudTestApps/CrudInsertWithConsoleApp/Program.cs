522910e03abe88f8c7735e318f114f731c379704
mustafa sacli <mustasacli@hotmail.com>
2022-08-28 02:21:36 +0300

    test apps added into solution.

---
+++ CrudTestApps/CrudInsertWithConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudInsertWithConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
