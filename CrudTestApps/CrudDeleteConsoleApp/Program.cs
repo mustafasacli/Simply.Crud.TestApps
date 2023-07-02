836e550da0539878f98df6815d1bf8c0bd71387e
mustafa.sacli <mustasacli@hotmail.com>
2023-07-03 00:28:25 +0300

    crud delete app arranged.

--- CrudTestApps/CrudDeleteConsoleApp/Program.cs
+++ CrudTestApps/CrudDeleteConsoleApp/Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
+using Simply.Common;
+using Simply.Crud;
+using Simply.Data.Database;
+using Simply.Data.Interfaces;
+using SimplyCrud.Project.Entity;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CrudDeleteConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static readonly string connectionString = "data source=127.0.0.1;initial catalog=crud_test_db;user id=root;";
+
+        private static void Main(string[] args)
         {
+            long id = args != null && args.Any() ? args[0].ToLong() : 0;
+            int result;
+            using (ISimpleDatabase database = new SimpleDatabase(
+                SimpleDatabase.Create<MySqlConnection>(connectionString)))
+            {
+                result = database.DeleteAll<Person>(q => q.Id == id);
+            }
+
+            Console.WriteLine($"result: {result}");
+            Console.ReadKey();
         }
     }
 }
