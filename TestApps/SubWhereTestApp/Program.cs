a682b07bccc1acd927a3e1eb6bfcd25485232e37
mustafasacli <mustasacli@hotmail.com>
2023-12-06 17:43:22 +0300

    subwhere test

--- TestApps/SubWhereTestApp/Program.cs
+++ TestApps/SubWhereTestApp/Program.cs
@@ -32,6 +32,7 @@ namespace SubWhereTestApp
 
             var cmd = whereClause.GetCommand();
             Console.WriteLine(cmd.CommandText);
+            Console.WriteLine("---------------------------");
 
             var field = whereClause.GetType()
                 .GetFields(BindingFlags.NonPublic |
@@ -41,7 +42,55 @@ namespace SubWhereTestApp
 
             ISimpleDatabase db = field.GetValue(whereClause) as ISimpleDatabase;
             Console.WriteLine(db.ToString());
+            Console.WriteLine("---------------------------");
             Console.ReadKey();
+
+            var whereClause2 = postgreSqlDatabase.Where<User>(/*tableAliasInfo: TableAliasInfo.New("u")*/)
+                 .AddAndCondition(a => a.IsActive == 1 && a.IsDeleted == 0)
+                 .SubWhere(q => q.UserType, ConditionTypes.And,
+                 SimpleSql.New("select ut2.id from user_types ut2 where ut2.is_active=?", new object[] { 1 })
+                 ).AddAllPropertiesForSelect();
+
+            var cmd2 = whereClause2.GetCommand();
+            Console.WriteLine(cmd2.CommandText);
+            Console.WriteLine("---------------------------");
+            Console.ReadKey();
+
+            var whereClause3 = postgreSqlDatabase.Where<User>(tableAliasInfo: TableAliasInfo.New("u3"))
+                 .AddAndCondition(a => a.IsActive == 1 && a.IsDeleted == 0)
+                 .SubWhere(SimpleSql.New("\"u3\".\"user_type\""), ConditionTypes.And,
+                 // Sub Where Clause Start
+                 postgreSqlDatabase.Where<UserType>(tableAliasInfo: TableAliasInfo.New("ut3"))
+                 .AddAndCondition(q => q.IsActive == 1)
+                 .AddPropertyForSelect(p => p.Id)
+                 // Sub Where Clause End)
+                 , isInOrNot: false
+                 ).AddAllPropertiesForSelect();
+
+            var cmd3 = whereClause3.GetCommand();
+            Console.WriteLine(cmd3.CommandText);
+            Console.WriteLine("---------------------------");
+            Console.ReadKey();
+
+            var whereClause4 = postgreSqlDatabase.Where<User>(tableAliasInfo: TableAliasInfo.New("u4"))
+                 .AddAndCondition(a => a.IsActive == 1 && a.IsDeleted == 0)
+                 .SubWhere(SimpleSql.New("\"u4\".\"user_type\""), ConditionTypes.And,
+                 SimpleSql.New("select ut4.id from user_types ut4 where ut4.is_active=?", new object[] { 1 })
+                 , isInOrNot: false
+                 ).AddAllPropertiesForSelect();
+
+            var cmd4 = whereClause4.GetCommand();
+            Console.WriteLine(cmd4.CommandText);
+            Console.WriteLine("---------------------------");
+            Console.ReadKey();
+
+            /*
+            var whereClause3 = whereClause.Union(whereClause2);
+            var cmd3 = whereClause3.GetCommand();
+            Console.WriteLine(cmd3.CommandText);
+            */
+            Console.ReadKey();
+
         }
     }
 }
